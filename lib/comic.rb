93aa95e9d0d06e2772772f6d3cae7325a70d5a6e
Cody Frank <cody.frank30@gmail.com>
2019-06-06 01:54:18 +0000

    set up cli class and comic class without methods

--- lib/comic.rb
+++ lib/comic.rb
@@ -1,1 +1,4 @@
 #this file will create a comic class that will be the data that makes up the comics
+
+class Comic
+end
--- lib/command-line-interface.rb
+++ lib/command-line-interface.rb
@@ -1,1 +1,4 @@
 #responsible for making a class to run the cli 
+
+class CommandLineInterface
+end
