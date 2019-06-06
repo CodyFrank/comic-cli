2f00dbc690f88a37c395c80d23239ba18b8d89de
Cody Frank <cody.frank30@gmail.com>
2019-06-06 01:42:18 +0000

    set up scraper file with notes for class responsibility

---
+++ lib/scraper.rb
@@ -0,0 +1,2 @@
+#this file will be responsible for scrapping https://www.marvel.com/comics?&options%5Boffset%5D=0&totalcount=12 for the latest release section
+#and scrapping https://www.marvel.com/comics/issue/77259/war_of_the_realms_new_agents_of_atlas_2019_3 (the links) for details about each comic release this week
