ef174d9c2854adfbb241ac2477044d197407355b
Cody Frank <cody.frank30@gmail.com>
2019-06-08 15:33:58 +0000

    parse for titles

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,10 +5,12 @@
 
 class Scraper
   
-  BASE_URL = "https://www.marvel.com/comics"
+  BASE_URL = "https://www.marvel.com/comics?byZone=marvel_site_zone&options[offset]=12&offset=0&options[limit]=12&options[formatType]=issue&options[noVariants]=true&options[dateDescriptor]=thisWeek&options[orderBy][0]=issues.onsale_date+DESC&options[orderBy][1]=issues.id+DESC&contentType=comic_issue&method=fetchByReleaseDate&options%5Boffset%5D=0&totalcount=24"
   
   def self.scrape_books
     html = open(BASE_URL)
+    doc = Nokogiri::HTML(html)
+    titles_parsed = doc.css("h2:contains('New Releases ')~div.JCMultiRow>div.row-item h5").text
     binding.pry
   end
 end
