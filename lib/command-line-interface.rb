39a122fe6f7dd90be37087936e8201840299756b
Cody Frank <cody.frank30@gmail.com>
2019-06-16 01:23:03 +0000

    formanting cli experiance

--- lib/command-line-interface.rb
+++ lib/command-line-interface.rb
@@ -1,22 +1,19 @@
 #responsible for making a class to run the cli 
 
 class CommandLineInterface
-  # note edge case inop still
+
   def run 
     input = nil
     self.intro
     Scraper.scrape_books
-    
     until input.to_s.downcase == "exit"
-      self.list_comics
-      input = gets.strip.downcase
-        if (1..Comic.all.length).include?(input.to_i)
-          input = self.dive(input.to_i-1)
-        elsif input == "exit"
-          puts "goodbye"
-        else
-          input = nil
-          puts "I do not understand the command. Please type a comic number or exit."
+    input = self.list_comics
+      if (1..Comic.all.length).include?(input.to_i)
+        input = self.dive(input.to_i-1)
+      elsif input == "exit"
+        puts "goodbye"
+      else
+        input = self.invalid_input
       end
     end
   end
@@ -25,14 +22,24 @@ class CommandLineInterface
   def intro 
     puts "Welcome to comics cli where you can see the latest in Marvel Comics\n" 
     puts "Remember you can always type 'exit' to back out\n"
-    puts "\n Lets check out whats new in Marvel comics this week!\n"
+    puts "\nLets check out whats new in Marvel comics this week!\n"
   end
   
   def list_comics
-    puts "\n Choose a number from (1 - #{Comic.all.length}) to see more info about.\n"
-    Comic.all.each_with_index do|issue, index|
-      puts "\n#{index + 1}. #{issue.title}\n"
+    puts "would you like to list the Comics?(y/n)"
+    input = gets.strip.downcase
+    if input == "y"
+      Comic.all.each_with_index do|issue, index|
+        puts "\n#{index + 1}. #{issue.title}"
+      end
+      puts "\n Choose a number from (1 - #{Comic.all.length}) to see more info.\n"
+      input = gets.strip.downcase
+    elsif input == "n" || input == "exit"
+      input = "exit"
+    else 
+      input = nil
     end
+    return input
   end
   
   def dive(index)
@@ -47,11 +54,18 @@ class CommandLineInterface
     puts "\n Description: - #{comic.description}\n\n"
     puts "Would you like to see other comics?(y/n)"
     input = gets.strip.downcase
-    if input.downcase == "y"
+    if input == "y"
       return "y"
     elsif input == "n" || input.downcase == "exit"
       puts "goodbye"
       return "exit"
+    else
+      self.invalid_input
     end
   end
+  
+  def invalid_input
+    puts "I do not understand the command."
+  end
+  
 end
